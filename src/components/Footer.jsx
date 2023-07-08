9cf8215b70e9dd033c1e4c73f78f9bb8f1cf91a3
yuvraj <yuvrajgoswami8182@gmail.com>
2023-07-08 18:38:39 +0530

    infotach

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>InfoTech</h1>
+            <p>@all right reserved</p>
+        </div>
+
+        <div>
+            <h5>Follow us </h5>
+            <div>
+                <a href='https://youtub.com/collectionhub' target={'blank'}>Youtub</a>
+                <a href='https://instgram.com/yuvraj_goswami_333' target={'blank'}>Instagram</a>
+                <a href='https://github.com/yuvraj-8182' target={'blank'}>Github</a>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
